17046b0531d8da13b06be67825430a295a887285
AvinashSoerdjbali <46559969+AvinashSoerdjbali@users.noreply.github.com>
2019-02-14 17:33:53 +0100

    Get en Post doet het

--- src/main/java/com/SET/Schaaktoernooi/API/SchakerEndpoint.java
+++ src/main/java/com/SET/Schaaktoernooi/API/SchakerEndpoint.java
@@ -5,6 +5,8 @@ import com.SET.Schaaktoernooi.Model.Schaker;
 import com.SET.Schaaktoernooi.controller.SchaakRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,17 +16,15 @@ public class SchakerEndpoint {
     private SchaakRepository mijnSchaakRepository;
 
     @GetMapping("geefalleschakers")
-    public Iterable<Schaker> geefalleschakers(){
+    public Iterable<Schaker> geefalleschakers() {
         return mijnSchaakRepository.findAll();
     }
 
+    @PostMapping("toevoegen")
+    public String toevoegen(@RequestBody Schaker schaker) {
+        mijnSchaakRepository.save(schaker);
+        return "het is gelukt";
 
 
-
-
-
-
-
-
-
+    }
 }
